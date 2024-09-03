8a155c6103ac69b3e16c1abdebd2fedead23b46f
eolus87 <niguvaz@gmail.com>
2024-09-03 19:41:49 +0100

    Fixed and corrected the power request function for tapo devices

--- sources/tapo_power.py
+++ sources/tapo_power.py
@@ -13,16 +13,20 @@ if platform.system() == 'Windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
-async def power_reading(target_ip: str, user_name: str, password: str) -> float:
+async def power_request(target_ip: str, user_name: str, password: str) -> float:
     # Set up the client and connect to the device
     client = ApiClient(user_name, password)
     device = await client.p110(target_ip)
 
     # Measure the current power
     current_power = await device.get_current_power()
-    current_power_float = float(current_power.to_dict()['power'])
+    current_power_float = float(current_power.to_dict()['current_power'])
 
     # Clean up
     del client, device
 
     return current_power_float
+
+def power_function(target_ip: str, user_name: str, password: str) -> float:
+    power = asyncio.run(power_request(target_ip, user_name, password))
+    return power
